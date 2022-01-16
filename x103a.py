090a06240875050c81ec6e6610e38b1d8743c353
fraxs <evanr561@deltalearns.ca>
2022-01-16 14:49:58 -0800

    Update x103a.py

--- x103a.py
+++ x103a.py
@@ -1,5 +1,5 @@
 #!python3
-
+import csv
 """
 Create a function called getData()
 getData takes no input parameters
@@ -7,14 +7,23 @@ return: file contents as a string
 
 """
 def getData():
-    return data
-
+    file = open('data.csv')
+    csvreader = csv.reader(file)
+    header = []
+    header = next(csvreader)
+    header = ''.join(header)
+    rows = []
+    for row in csvreader:
+        rows.append(row)
+    rows = header.join(rows)
+    list1 = [header,rows]
+    return list1
 def main():
     data = getData()
-    assert "list" in str(type(db))
-    assert "list" in str(type(db[0]))
-    assert "Placed in Service" in data[0] == True
-    assert "MAC 6c299551c1b5" in data[33] == True    
+    assert ("list" in str(type(data)))
+    assert ("list" in str(type(data[0])))
+    assert ("Placed in Service" in data[0]) == True
+    assert ("MAC 6c299551c1b5" in data[33]) == True    
     assert data.find("No.,Equipment Item N") == 0
     assert data.find("Barry") == 27089
 
